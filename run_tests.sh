1a26fd05b1d5719862b521ff41eda4a5d7b86314
Taavi Päll <tapa741@gmail.com>
2020-10-22 13:11:54 +0300

    rm some tests

--- run_tests.sh
+++ run_tests.sh
@@ -15,9 +15,5 @@ set -x
 # Verify R version
 singularity exec R -q -e "stopifnot(getRversion() == '$(version)')"
 
-# Install library
-singularity exec R -q -e "install.packages('dplyr', dependencies = TRUE)"
-singularity exec "${SINGULARITY_IMAGE}" install2.r --error --deps TRUE dplyr
-
 { set +x; } 2>/dev/null
 echo "All tests passed!"
